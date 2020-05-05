d23c3a9af1437f29853ff88b09fda166ecd7f023
Pegazeus-Victor <61429368+Pegazeus-Victor@users.noreply.github.com>
2020-05-05 13:51:21 -0700

    Updates

---
+++ precache-manifest.6506fd195ad2eeb555df9ee82fd55ebd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a209b0d8dde31b41aa09e877a41a4e26",
+    "url": "/AirBNBSpaceX/index.html"
+  },
+  {
+    "revision": "3bf7535f801ffc3a9a0e",
+    "url": "/AirBNBSpaceX/static/js/2.7c64cff7.chunk.js"
+  },
+  {
+    "revision": "a279015ada6827c525c898fa55497f39",
+    "url": "/AirBNBSpaceX/static/js/2.7c64cff7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa15b2ee4c4eea48eb52",
+    "url": "/AirBNBSpaceX/static/js/main.f4ab7d83.chunk.js"
+  },
+  {
+    "revision": "3798fe0d3ab7946aef77",
+    "url": "/AirBNBSpaceX/static/js/runtime-main.ca56d409.js"
+  },
+  {
+    "revision": "07d5ba4a894c77d6f799ecde7e84850f",
+    "url": "/AirBNBSpaceX/static/media/cart.07d5ba4a.svg"
+  },
+  {
+    "revision": "a8a3ba3a10003218c2a017cf68d20489",
+    "url": "/AirBNBSpaceX/static/media/curve.a8a3ba3a.svg"
+  },
+  {
+    "revision": "f25b4cba48a496093085f268698a4b39",
+    "url": "/AirBNBSpaceX/static/media/dog-1.f25b4cba.png"
+  },
+  {
+    "revision": "ab7a533bb445ea0d167a34cdc0eec387",
+    "url": "/AirBNBSpaceX/static/media/dog-2.ab7a533b.png"
+  },
+  {
+    "revision": "b62803749fd80890a425b0dfae080c47",
+    "url": "/AirBNBSpaceX/static/media/dog-3.b6280374.png"
+  },
+  {
+    "revision": "98f2a8b1296f385203e7b9b5d9938fcf",
+    "url": "/AirBNBSpaceX/static/media/exit.98f2a8b1.svg"
+  },
+  {
+    "revision": "0b1c67758d564d5eb140740847e6886e",
+    "url": "/AirBNBSpaceX/static/media/galaxy.0b1c6775.jpg"
+  },
+  {
+    "revision": "3192f86cb3a13b08200f5e3d3fef0325",
+    "url": "/AirBNBSpaceX/static/media/home.3192f86c.svg"
+  },
+  {
+    "revision": "48932214f494cf346794f464ee2c04e3",
+    "url": "/AirBNBSpaceX/static/media/iss.48932214.jpg"
+  },
+  {
+    "revision": "0c7bf061e88c942e5031e971c6ab447b",
+    "url": "/AirBNBSpaceX/static/media/logo.0c7bf061.svg"
+  },
+  {
+    "revision": "493bbb752333aacb0e80bab31c2e925a",
+    "url": "/AirBNBSpaceX/static/media/moon.493bbb75.jpg"
+  },
+  {
+    "revision": "ea589b574fff44db577387da01243704",
+    "url": "/AirBNBSpaceX/static/media/profile.ea589b57.svg"
+  },
+  {
+    "revision": "c27cc1f53fed31cbbc1d743f9bbaba1d",
+    "url": "/AirBNBSpaceX/static/media/rocket.c27cc1f5.svg"
+  },
+  {
+    "revision": "7dfc79232cbb090253471fe744fe5680",
+    "url": "/AirBNBSpaceX/static/media/space.7dfc7923.jpg"
+  }
+]);
